9018640b380b7448e4dffc76772bc6ba02c416ae
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-08-13 19:25:46 -0500

    set up item class and added items to rooms

--- src/player.py
+++ src/player.py
@@ -3,17 +3,22 @@
 
 from room import Room
 
-# MIght reuse this to allow users to type full direction values instead of n w e s
-direction_names = {
-    'n': 'North',
-    'e': 'East',
-    'w': 'West',
-    's': 'South'
-}
+
 
 class Player:
     def __init__(self, name, room):
         self.name = name
         self.room = room
+        self.inventory = []
 
     
+    def __str__(self):
+        return f"Player: {self.name}, {self.room}"
+
+    
+    def takeItem(self, item):
+        self.inventory.append(item)
+
+
+    def drop(self, item):
+        del self.inventory[item]
--- src/room.py
+++ src/room.py
@@ -1,15 +1,29 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+from item import Item
 class Room():
-    def __init__(self, name, description):
+    def __init__(self, name, description, item):
         self.name = name
         self.description = description
+        self.item = [item]
      
 
+def __str___(self):
+    return f"Room: {self.name}, Description: {self.description}"
+
+
+def remove_item(self, item):
+    del self.item[item]
+
+
+def add_item(self, item):
+    self.item.append(item)
 
 # * The room should also have `n_to`, `s_to`, `e_to`, and `w_to` attributes
     #which point to the room in that respective direction.
 
     #Realized this was, along with the pre populated rooms, set with the rooms
     #All set up in adv.py lmao
+
+    # MIght reuse this to allow users to type full direction values instead of n w e s
